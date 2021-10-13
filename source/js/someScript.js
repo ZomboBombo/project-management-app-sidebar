0f422d27400dba4afd62995a0f0473077a08c174
Sergey Novikov <foxinazombobombo@mail.ru>
2021-10-13 23:24:17 +0300

    Remove unnecessary files

--- source/js/someScript.js
+++
@@ -1,7 +0,0 @@
-let counter = 0;
-
-export default function onClick () {
-  counter++;
-  console.log('Counter value:', counter);
-  console.log('Bugaga-ga!:');
-};
--- source/js/vendor/thisIsVendor.js
+++
@@ -1,6 +0,0 @@
-let name = 'I am a Vendor!';
-let lastName = 'I am a Vendor lastName!';
-console.log('It is working!');
-console.log('It is working!');
-console.log('It is working!');
-console.log('It is working!');
