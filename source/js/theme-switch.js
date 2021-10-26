deeb07aec3dbaa9c1704886018383b0f4999b0ca
Sergey Novikov <foxinazombobombo@mail.ru>
2021-10-26 14:40:32 +0300

    Add logic to save theme state to localStorage

--- source/js/theme-switch.js
+++ source/js/theme-switch.js
@@ -2,9 +2,26 @@ import Utils from './utils';
 
 
 // ---------- КОНСТАНТЫ -----------
+const LOCAL_STORAGE_THEME_STATE = 'Current theme state';
+const ALTERNATIVE_THEME_CLASS = 'light-theme';
+const THEME_SWITCHER_ACTIVE_CLASS_MOD = 'theme-switcher__button--active';
+
+/*
+ * --- Словарик для названий цветовых Тем:
+ * --- ---
+ * --- * для "интеграции" с содержимым подписи к кнопке;
+ * --- * для записи значений в localStorage;
+ */
 const ThemeName = {
-  DEFAULT: 'Тёмную',
-  ALTERNATIVE: 'Светлую',
+  text_content: {
+    DEFAULT: 'Тёмную',
+    ALTERNATIVE: 'Светлую',
+  },
+
+  local_storage: {
+    DEFAULT: 'Default',
+    ALTERNATIVE: 'Alternative',
+  },
 };
 
 
@@ -22,23 +39,39 @@ const themeSwitcher = themeSwitcherBlock.querySelector('#theme-switcher');
 =======================================================================
 */
 
+/*
+ *** Условие для проверки текущего состояния Темы:
+ *** ---
+ *** ЕСЛИ запись в localStorage равняется "...ALTERNATIVE",
+ *** ТО установить альтернативную ("Светлую") тему на сайте. 
+ */
+const currentThemeState = localStorage.getItem(LOCAL_STORAGE_THEME_STATE);
+if (currentThemeState === ThemeName.local_storage.ALTERNATIVE) {
+  body.setAttribute('class', ALTERNATIVE_THEME_CLASS);
+  themeNameTextBlock.textContent = ThemeName.text_content.DEFAULT;
+  themeSwitcher.classList.add(THEME_SWITCHER_ACTIVE_CLASS_MOD);
+}
+
+
 // *** Функция для обработчика события клика на кнпоку переключения темы ***
 const onThemeSwitcherClick = (evt) => {
   evt.preventDefault();
 
-  themeSwitcher.classList.toggle('theme-switcher__button--active');
+  themeSwitcher.classList.toggle(THEME_SWITCHER_ACTIVE_CLASS_MOD);
 
   switch (Utils.constant.IS_TRUE) {
-    case themeSwitcher.classList.contains('theme-switcher__button--active'):
-      body.setAttribute('class', 'light-theme');
-      themeNameTextBlock.textContent = ThemeName.DEFAULT;
-      themeSwitcher.classList.add('theme-switcher__button--active');
+    case themeSwitcher.classList.contains(THEME_SWITCHER_ACTIVE_CLASS_MOD):
+      body.setAttribute('class', ALTERNATIVE_THEME_CLASS);
+      themeNameTextBlock.textContent = ThemeName.text_content.DEFAULT;
+      themeSwitcher.classList.add(THEME_SWITCHER_ACTIVE_CLASS_MOD);
+      localStorage.setItem(LOCAL_STORAGE_THEME_STATE, ThemeName.local_storage.ALTERNATIVE);
       break;
 
     default:
       body.removeAttribute('class');
-      themeNameTextBlock.textContent = ThemeName.ALTERNATIVE;
-      themeSwitcher.classList.remove('theme-switcher__button--active');
+      themeNameTextBlock.textContent = ThemeName.text_content.ALTERNATIVE;
+      themeSwitcher.classList.remove(THEME_SWITCHER_ACTIVE_CLASS_MOD);
+      localStorage.setItem(LOCAL_STORAGE_THEME_STATE, ThemeName.local_storage.DEFAULT);
   }
 };
 
